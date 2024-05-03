e24397a097ccdd6a29de8c4d3bb9a7218a303b19
j-at-ch <107470925+j-at-ch@users.noreply.github.com>
2024-05-03 19:27:37 +0100

    Add library.py and 1.py

---
+++ 1.py
@@ -0,0 +1,22 @@
+import argparse
+import numpy as np
+from library import arithmetic_sum
+
+
+def solution(n):
+    """
+    Use inclusion exclusion together with the exact formula for arithmetic sums.
+    """
+    m = n - 1
+    s_3 = 3 * arithmetic_sum(np.floor(m / 3))
+    s_5 = 5 * arithmetic_sum(np.floor(m / 5))
+    s_15 = 15 * arithmetic_sum(np.floor(m / 15))
+    return s_3 + s_5 - s_15
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n', type=int, default=1000)
+    args = parser.parse_args()
+
+    print(f'The answer for n={args.n} is {solution(args.n)}')
---
+++ library.py
@@ -0,0 +1,15 @@
+from functools import cache
+
+
+@cache
+def factorial(n):
+    return n * factorial(n-1) if n != 1 else 1
+
+
+@cache
+def sum_factorials(n: int):
+    return factorial(n) + sum_factorials(n - 1) if n != 1 else 1
+
+
+def arithmetic_sum(n: int) -> int:
+    return int(0.5 * n * (n + 1))
