c83fb79652974c303c130b2eb03f3eb78a093325
j-at-ch <107470925+j-at-ch@users.noreply.github.com>
2024-05-04 02:24:51 +0100

    Solve 10.py (needs efficiency improvements)

---
+++ solutions/10.py
@@ -0,0 +1,26 @@
+import argparse
+from library import has_divisor
+
+
+def solution(n):
+    prime_list = []
+    i = 0
+    q = 2
+    while q < n:
+        if q % 1000 == 0:
+            print(q)
+        if not has_divisor(q, prime_list):
+            prime_list.append(q)
+            i += 1
+        q += 1
+    return sum(prime_list)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser('Problem 10: Summation of Primes')
+    parser.add_argument('--n', type=int, default=10, help='Expects 17')
+    args = parser.parse_args()
+
+    n = args.n
+    answer = solution(n)
+    print(f'The answer for the PROBLEM n={args.n} is {answer}')
