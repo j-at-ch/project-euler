bbe51a9033265ab4899ab00071ef465c65076551
j-at-ch <107470925+j-at-ch@users.noreply.github.com>
2024-07-05 23:58:38 +0100

    Big DP bump for solution to 082.py

--- solutions/082.py
+++ solutions/082.py
@@ -1,5 +1,4 @@
 import argparse
-import numpy as np
 
 
 def solution():
@@ -7,30 +6,22 @@ def solution():
     with open('data/082.txt') as f:
         arr = [[int(a) for a in r.split(',')] for r in f.read().strip('\n').split('\n')]
 
-    n_rows = len(arr)
-    n_cols = len(arr[0])
+    m = len(arr)
+    n = len(arr[0])
 
-    arr = np.array(arr)
-    dp = np.zeros_like(arr, dtype=float)
-    dp[:, 0] = arr[:, 0]
+    dp = [[float('inf')] * (n + 2) for _ in range(m + 2)]
 
-    for j in range(1, n_cols):
-        for i in range(n_rows):
-            tmp_ij = np.inf
-            for k in range(n_rows):
-                if i < k:
-                    idx_st = i + 1
-                    idx_end = k + 1
-                elif i > k:
-                    idx_st = k
-                    idx_end = i
-                else:
-                    idx_st = idx_end = k
-                path_sum = dp[k, j - 1] + arr[idx_st:idx_end, j].sum()
-                tmp_ij = min(tmp_ij, path_sum)
-            dp[i, j] = arr[i, j] + tmp_ij
+    for i in range(1, m + 1):
+        dp[i][1] = arr[i - 1][0]
 
-    return int(min(dp[:, -1]))
+    for j in range(2, n + 1):
+        for i in range(1, m + 1):
+            dp[i][j] = arr[i - 1][j - 1] + min(dp[i - 1][j], dp[i][j - 1])
+
+        for i in range(m, 0, -1):
+            dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + min(dp[i + 1][j], dp[i][j + 1]))
+
+    return int(min([dp[i][m] for i in range(m + 1)]))
 
 
 if __name__ == '__main__':
