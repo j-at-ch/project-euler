c60e31952184c3cd49b58d6c9a16893dc2db3b74
j-at-ch <107470925+j-at-ch@users.noreply.github.com>
2024-07-06 09:17:01 +0100

    Update 083.py - did not previously iterate to convergence!

--- solutions/083.py
+++ solutions/083.py
@@ -4,7 +4,7 @@ import argparse
 def solution():
     """
     DP to the rescue! Needs some improvement, but the idea is to sweep from L2R checking up and down paths as
-    for 082, then sweep R2L up and down, then a final sweep L2R up and down.
+    for 082, then sweep R2L up and down, and iterate to convergence.
     """
     with open('data/083.txt') as f:
         arr = [[int(a) for a in r.split(',')] for r in f.read().strip('\n').split('\n')]
@@ -13,25 +13,20 @@ def solution():
     n = len(arr[0])
     dp = [[float('inf')] * (n + 2) for _ in range(m + 2)]
 
-    for j in range(1, n + 1):
-        for i in range(1, m + 1):
-            path_part = 0 if i * j == 1 else min(dp[i - 1][j], dp[i][j - 1])
-            dp[i][j] = arr[i - 1][j - 1] + path_part
+    dp_prev = None
+    while dp != dp_prev:
+        dp_prev = [r[:] for r in dp]
+        for j in range(1, n + 1):
+            for i in range(1, m + 1):
+                path_part = 0 if i * j == 1 else min(dp[i - 1][j], dp[i][j - 1])
+                dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + path_part)
 
-        for i in range(m, 0, -1):
-            dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + min(dp[i + 1][j], dp[i][j + 1]))
+            for i in range(m, 0, -1):
+                dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + min(dp[i + 1][j], dp[i][j + 1]))
 
-    for j in range(m, 0, -1):
-        for i in range(1, m + 1):
-            dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + dp[i][j + 1])
-
-    for j in range(1, n + 1):
-        for i in range(1, m + 1):
-            path_part = 0 if i * j == 1 else min(dp[i - 1][j], dp[i][j - 1])
-            dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + path_part)
-
-        for i in range(m, 0, -1):
-            dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + min(dp[i + 1][j], dp[i][j + 1]))
+        for j in range(m, 0, -1):
+            for i in range(1, m + 1):
+                dp[i][j] = min(dp[i][j], arr[i - 1][j - 1] + min(dp[i + 1][j], dp[i][j + 1]))
 
     return dp[m][n]
 
